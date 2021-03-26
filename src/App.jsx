a9277674db0be73ef9aaf81bad70591f3e753ac4
Arindam Dutta <arindam408@gmail.com>
2021-03-27 01:30:49 +0530

    static portion done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import React from 'react';
+import ContainerOne from './components/ContainerOne';
+
+function App() {
+  return (
+    <div id='screen'>
+    <Header/>
+    <ContainerOne/>
+    </div>
+  );
+}
+
+export default App;
